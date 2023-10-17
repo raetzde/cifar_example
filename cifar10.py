d4d46431fa5301f8110b9c76037d5ffc7e76f98f
Detlef Raetz <detlef@dr-raetz.de>
2023-10-17 09:09:26 +0200

    Version 2.2

--- cifar10.py
+++ cifar10.py
@@ -7,6 +7,7 @@ import torchvision.transforms as transforms
 
 from utils import visualize_images
 # Test
+# Quellcodeänderung von Rätz
 # Pfad in welches das fertige Model gespeichert wird und von welchem ein trainiertes Model geladen wird
 MODEL_PATH = './cifar_net_2epochs.pth'
 # Auf False setzen, wenn man das Model nicht erneut trainieren will, sondern nur ein bestehendes Model testen will
